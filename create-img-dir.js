3c3de0479e656acf5be38f7483c9ab06ab603e27
CrisLorenzo07 <sscristian77@gmail.com>
2025-09-10 00:17:36 -0300

    Se eliminan archivos duplicados

--- create-img-dir.js
+++
@@ -1,8 +0,0 @@
-// Crear directorio para imágenes de perfil si no existe
-import fs from "fs";
-import path from "path";
-
-const publicDir = "public/img";
-if (!fs.existsSync(publicDir)) {
-    fs.mkdirSync(publicDir, { recursive: true });
-}
