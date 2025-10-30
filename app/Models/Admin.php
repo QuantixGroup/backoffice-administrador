72d2f3edb228543707d795d5ab81434644a2c189
CrisLorenzo07 <sscristian77@gmail.com>
2025-10-30 13:37:16 -0300

    Se actualiza la configuración de autenticación y db. | Se añade el modelo Admin para evitar errores

---
+++ app/Models/Admin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'admins';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'apellido',
+        'cedula',
+        'email',
+        'password',
+        'telefono',
+        'fecha_nacimiento',
+        'profile_image',
+        'fecha_ingreso',
+        'fecha_egreso',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'fecha_nacimiento' => 'date',
+            'fecha_ingreso' => 'date',
+            'fecha_egreso' => 'date',
+        ];
+    }
+}
